75fdc70371ac8807e00d3bfc306960b2c3fc5a8e
Hossein Salimi <46236489+hosseincomedian@users.noreply.github.com>
2023-06-13 11:27:30 +0330

    set completed todo

--- core/todo/urls.py
+++ core/todo/urls.py
@@ -1,8 +1,9 @@
 
 
 from django.urls import path, include
-from .views import TodoListView, TodoCreateView
+from .views import TodoListView, TodoCreateView, TodoCompleteView
 urlpatterns = [
     path('', TodoListView.as_view(), name='todo-list'),
     path('create/', TodoCreateView.as_view(), name='todo-create'),
+    path('complete/', TodoCompleteView.as_view(), name='todo-complete'),
 ]
--- core/todo/views.py
+++ core/todo/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, DeleteView
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, View
 from .models import Todo
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.http import HttpResponse
 
 class TodoListView(LoginRequiredMixin, ListView):
     model = Todo
+
+    def get_queryset(self):
+        return self.model.objects.filter(user = self.request.user)
 
 
 class TodoCreateView(LoginRequiredMixin, CreateView):
@@ -18,3 +22,16 @@ class TodoCreateView(LoginRequiredMixin, CreateView):
         obj.user = self.request.user
         obj.save()        
         return super().form_valid(form)
+
+class TodoCompleteView(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        todo = Todo.objects.get(pk=request.POST.get('todo_id'))
+        todo.complete = not todo.complete
+        todo.save()
+        return HttpResponse("ok")
+
+class TodoUpdateView(UpdateView):
+    pass
+
+class TodoDeleteView(DeleteView):
+    pass
